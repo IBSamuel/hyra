bb3b851286324d26f44df968c5442f65a3291732
Adeousn Samuel Oluwatomisin <ibsam26700749@gmail.com>
2024-05-16 15:15:13 +0100

    i fixed vercel error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navbar from './home_comps/Navbar'
+import Header from './home_comps/Header'
+import Choose from './home_comps/Choose'
+import Teachings from './home_comps/Teachings'
+import SubFooter from './home_comps/SubFooter'
+import Footer from './home_comps/Footer'
+
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+        <Navbar/>
+        <Header/>
+        <Choose/>
+        <Teachings/>
+        <SubFooter/>
+        <Footer/>
+    </>
   )
 }
 
